7cc1bd94c8b387e89631c87c6c19bec48534fc8e
aoronchu <aoronchu@gmail.com>
2021-02-25 19:40:16 +0800

    add config.toml

--- scaffold-core/src/main/java/com/webank/scaffold/core/artifact/ConfDir.java
+++ scaffold-core/src/main/java/com/webank/scaffold/core/artifact/ConfDir.java
@@ -1,8 +1,10 @@
 package com.webank.scaffold.core.artifact;
 
 import com.webank.scaffold.core.DirectoryCopier;
+import com.webank.scaffold.core.util.IOUtil;
 
 import java.io.File;
+import java.io.InputStream;
 
 /**
  * @author aaronchu
@@ -13,12 +15,21 @@ public class ConfDir extends DirectoryArtifact {
 
     private static final String CONF_DIR = "conf";
 
+    private static final String TEMPLATE_RESOURCE
+            = "templates/config.toml";
+    private static final String CONFIG_TOML = "config.toml";
+
     public ConfDir(File path) {
         super(path);
     }
 
     @Override
     protected void doGenerateSubContents() throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try(InputStream in = classLoader.getResourceAsStream(TEMPLATE_RESOURCE)){
+            String configContent = IOUtil.readAsString( in );
+            IOUtil.writeString(new File(this.toFile(), CONFIG_TOML),configContent);
+        }
     }
 
     @Override
